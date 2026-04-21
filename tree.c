57cf8e01251c824f3c19929fd991d5a34de37bbf
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-21 23:38:04 +0530

    Write tree object to object store

--- tree.c
+++ tree.c
@@ -150,4 +150,7 @@ int tree_from_index(ObjectID *id_out) {
     void *data;
     size_t len;
     if (tree_serialize(&tree, &data, &len) != 0) return -1;
+
+    int rc = object_write(OBJ_TREE, data, len, id_out);
+    free(data);
 }
