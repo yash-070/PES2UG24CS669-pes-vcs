3740347b67d8cbc5239e2af1b3bda76f907ae9cc
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-21 23:16:01 +0530

    Add header creation logic for object format

--- object.c
+++ object.c
@@ -105,7 +105,12 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
                            (type == OBJ_TREE) ? "tree" : "commit";
     int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
 
-    
+     size_t total_len = header_len + len;
+    uint8_t *buffer = malloc(total_len);
+    if (!buffer) return -1;
+
+    memcpy(buffer, header, header_len);
+    memcpy(buffer + header_len, data, len);
  
     free(buffer);
     return 0;
