82ff75f23590ddd8f3035a4cab9ddbfef2c01109
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-21 23:49:42 +0530

    Phase 4: initialize commit_create structure

--- commit.c
+++ commit.c
@@ -196,6 +196,6 @@ int head_update(const ObjectID *new_commit) {
 int commit_create(const char *message, ObjectID *commit_id_out) {
     // TODO: Implement commit creation
     // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+   
+    
 }
