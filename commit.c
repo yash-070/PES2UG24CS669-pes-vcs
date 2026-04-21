6d5d9b252469e4c854fc3931f96ae89e068c76a3
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-21 23:53:49 +0530

    Write commit object and update HEAD reference

--- commit.c
+++ commit.c
@@ -218,4 +218,13 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     void *data;
     size_t len;
     if (commit_serialize(&c, &data, &len) != 0) return -1;
+
+     if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    return head_update(commit_id_out);
 }
