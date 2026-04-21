96c4f24f5533f977234af92c9574db3576ffa75c
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-21 23:14:56 +0530

    Phase 1: initialize object_write structure

--- object.c
+++ object.c
@@ -100,21 +100,13 @@ int object_exists(const ObjectID *id) {
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
 
-      char header[64];
+       char header[64];
     const char *type_str = (type == OBJ_BLOB) ? "blob" :
                            (type == OBJ_TREE) ? "tree" : "commit";
     int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
 
-    size_t total_len = header_len + len;
-    uint8_t *buffer = malloc(total_len);
-    if (!buffer) return -1;
-
-    memcpy(buffer, header, header_len);
-    memcpy(buffer + header_len, data, len);
-
-
-    compute_hash(buffer, total_len, id_out);
-     
+    
+ 
     free(buffer);
     return 0;
     
