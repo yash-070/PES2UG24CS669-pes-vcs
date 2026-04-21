d5123919f6866b6eb9f0a64f9c716c2eff289549
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-21 23:34:32 +0530

    Phase 2: initialize tree_from_index

--- tree.c
+++ tree.c
@@ -132,6 +132,7 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 int tree_from_index(ObjectID *id_out) {
     // TODO: Implement recursive tree building
     // (See Lab Appendix for logical steps)
-    (void)id_out;
-    return -1;
+    Index index;
+
+    
 }
