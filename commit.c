5f4967783e4feff5b0aa09bd9d8b92f62767c9e2
yashas <yashas@ubuntu.myguest.virtualbox.org>
2026-04-22 01:31:44 +0530

    .

--- commit.c
+++ commit.c
@@ -226,5 +226,6 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     free(data);
 
-    return head_update(commit_id_out);
+     if (head_update(commit_id_out) != 0) return -1;
+     return 0;
 }
